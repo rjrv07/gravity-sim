77edc6a31d1464a9b8adc5d1ceaa0ff632708e67
Roelie03 <109596367+Roelie03@users.noreply.github.com>
2025-03-17 09:11:40 +0200

    Reformatting main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,12 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+int main() {
+    auto window = sf::RenderWindow(sf::VideoMode({1500u, 800u}), "CMake SFML Project");
+    window.setFramerateLimit(120);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
