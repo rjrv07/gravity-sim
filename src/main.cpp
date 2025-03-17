56bdaf7dff8cf19a92ca071cefe5f925db481f25
Roelie03 <109596367+Roelie03@users.noreply.github.com>
2025-03-17 09:23:40 +0200

    Random number generator

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
+#include <random>
 #include <SFML/Graphics.hpp>
+
+float getRandomSignedFloat(float min = -1000.f, float max = 1000.f) {
+    static std::random_device rd; // Seed for random number engine
+    static std::mt19937 gen(rd()); // Mersenne Twister PRNG
+    std::uniform_real_distribution<float> dist(min, max); // Uniform distribution
+
+    return dist(gen); // Generate a random float in [min, max]
+}
 
 int main() {
     auto window = sf::RenderWindow(sf::VideoMode({1500u, 800u}), "CMake SFML Project");
